b64edac1d0b32e28854de85a650b5b1dcbf78606
sayro <sayrodamaceno70@gmail.com>
2025-12-02 20:37:31 -0300

    feat: enhance Header component with session management and improved NavLinks

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import { useState } from 'react'
 import Link from 'next/link'
 import {
   Sheet,
@@ -11,27 +11,50 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet'
 import { Button } from '@/components/ui/button'
-import { Menu } from 'lucide-react'
+import { LogIn, Menu } from 'lucide-react'
 
 const navItems = [{ href: '#profissionais', label: 'Profissionais' }]
 
-const NavLinks = ({ setIsOpen }: { setIsOpen: (open: boolean) => void }) => (
+const NavLinks = ({
+  setIsOpen,
+  session,
+}: {
+  setIsOpen: (open: boolean) => void
+  session: boolean
+}) => (
   <>
     {navItems.map((item) => (
       <Button
         key={item.href}
         asChild
-        className="bg-transparent hover:bg-transparent text-black shadow-none"
+        className="bg-transparent hover:bg-transparent text-black shadow-none text-base font-normal"
         onClick={() => setIsOpen(false)}
       >
-        <Link href={item.href}>{item.label}</Link>
+        <Link href={item.href} className="">
+          {item.label}
+        </Link>
       </Button>
     ))}
+
+    {session ? (
+      <Link
+        href="/dashboard"
+        className="flex items-center justify-center gap-2"
+      >
+        Acessar clínica
+      </Link>
+    ) : (
+      <Button>
+        <LogIn />
+        Portal da clínica
+      </Button>
+    )}
   </>
 )
 
 export function Header() {
-  const [isOpen, setIsOpen] = React.useState(false)
+  const [isOpen, setIsOpen] = useState(false)
+  const [session] = useState(null)
 
   return (
     <header className="fixed top-0 left-0 right-0 z-999 py-4 px-6 bg-white">
@@ -41,7 +64,7 @@ export function Header() {
         </Link>
 
         <nav className="hidden md:flex items-center space-x-4">
-          <NavLinks setIsOpen={setIsOpen} />
+          <NavLinks setIsOpen={setIsOpen} session={!!session} />
         </nav>
 
         <Sheet open={isOpen} onOpenChange={setIsOpen}>
@@ -61,7 +84,7 @@ export function Header() {
             <SheetDescription>Veja nossos links</SheetDescription>
 
             <nav className="flex flex-col space-y-4">
-              <NavLinks setIsOpen={setIsOpen} />
+              <NavLinks setIsOpen={setIsOpen} session={!!session} />
             </nav>
           </SheetContent>
         </Sheet>
