3bc26020f6b12d8a4a66f1e24d6c9718b9c0534c
sayro <sayrodamaceno70@gmail.com>
2025-12-06 18:09:33 -0300

    feat: add authentication handlers and configure NextAuth with GitHub provider

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-};
+  images: {
+    qualities: [100, 75],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
