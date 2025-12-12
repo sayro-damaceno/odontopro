54ca49c2b43c0beb6afd72c9dfae4db96a0cef48
sayro <sayrodamaceno70@gmail.com>
2025-12-12 19:54:42 -0300

    feat: implement services list with currency formatting and action buttons

--- src/app/(panel)/dashboard/services/_components/services-list.tsx
+++ src/app/(panel)/dashboard/services/_components/services-list.tsx
@@ -2,13 +2,21 @@
 
 import { useState } from 'react'
 import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
-import { Card, CardHeader, CardTitle } from '@/components/ui/card'
-import { Plus } from 'lucide-react'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Pencil, Plus, X } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { DialogService } from './dialog-service'
+import { Service } from '@/generated/prisma/client'
+import { formatCurrency } from '@/utils/formatCurrency'
 
-export function ServicesList() {
+interface ServicesListProps {
+  services: Service[]
+}
+
+export function ServicesList({ services }: ServicesListProps) {
   const [isDialogOpen, setIsDialogOpen] = useState(false)
+
+  console.log(services)
 
   return (
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
@@ -28,6 +36,34 @@ export function ServicesList() {
               <DialogService closeModal={() => setIsDialogOpen(false)} />
             </DialogContent>
           </CardHeader>
+
+          <CardContent>
+            <section className="space-y-4">
+              {services.map((service) => (
+                <article
+                  key={service.id}
+                  className="flex items-center justify-between"
+                >
+                  <div className="flex items-center space-x-2">
+                    <span className="font-medium">{service.name}</span>
+                    <span className="text-gray-500">-</span>
+                    <span className="text-gray-400">
+                      {formatCurrency(service.price / 100)}
+                    </span>
+                  </div>
+
+                  <div>
+                    <Button variant="ghost" size="icon">
+                      <Pencil />
+                    </Button>
+                    <Button variant="ghost" size="icon">
+                      <X />
+                    </Button>
+                  </div>
+                </article>
+              ))}
+            </section>
+          </CardContent>
         </Card>
       </section>
     </Dialog>
---
+++ src/utils/formatCurrency.ts
@@ -0,0 +1,9 @@
+const CURRENCY_FORMATTER = new Intl.NumberFormat('pt-BR', {
+  currency: 'BRL',
+  style: 'currency',
+  minimumFractionDigits: 0,
+})
+
+export function formatCurrency(amount: number) {
+  return CURRENCY_FORMATTER.format(amount)
+}
