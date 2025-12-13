05e5b33b112c38d36ef64398e3506dcb37952903
sayro <sayrodamaceno70@gmail.com>
2025-12-13 18:25:46 -0300

    feat: add schedule page and content components with user data retrieval

---
+++ src/app/(public)/clinica/[id]/_components/schedule-content.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import Image from 'next/image'
+import imgTeste from '@/../public/foto1.png'
+import { MapPin } from 'lucide-react'
+
+export function ScheduleContent() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="h-32 bg-emerald-500" />
+
+      <section className="container mx-auto px-4 -mt-16">
+        <div className="max-w-2xl mx-auto">
+          <article className="flex flex-col items-center">
+            <div className="relative w-48 h-48 rounded-full overflow-hidden border-4 border-white mb-8">
+              <Image
+                src={imgTeste}
+                alt="Foto da clinica"
+                className="object-cover"
+                fill
+              />
+            </div>
+
+            <h1 className="text-2xl font-bold mb-2">Clinica teste</h1>
+            <div className="flex items-center gap-1">
+              <MapPin className="w-5 h-5" />
+              <span>Endereço da clinica</span>
+            </div>
+          </article>
+        </div>
+      </section>
+    </div>
+  )
+}
---
+++ src/app/(public)/clinica/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { redirect } from 'next/navigation'
+import { getInfoSchedule } from './_data-access/get-info-schedule'
+import { ScheduleContent } from './_components/schedule-content'
+
+export default async function SchedulePage({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const userId = (await params).id
+
+  const user = await getInfoSchedule({ userId })
+
+  if (!user) {
+    redirect('/')
+  }
+
+  console.log(user)
+
+  return <ScheduleContent />
+}
