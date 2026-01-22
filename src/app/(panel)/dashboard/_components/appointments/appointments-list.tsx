413e62026f3e7b99f2413f72a4ab031554fcdc4c
sayro <sayrodamaceno70@gmail.com>
2026-01-21 22:02:35 -0300

    feat: integrate react-query for appointment data fetching and add QueryClientProvider

--- src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
+++ src/app/(panel)/dashboard/_components/appointments/appointments-list.tsx
@@ -2,7 +2,14 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { Prisma } from '@/generated/prisma/client'
+import { useQuery } from '@tanstack/react-query'
+import { format } from 'date-fns'
 import { useSearchParams } from 'next/navigation'
+
+type AppointmentWithService = Prisma.AppointmentGetPayload<{
+  include: { service: true }
+}>
 
 interface AppointmentsListProps {
   times: string[]
@@ -11,7 +18,53 @@ interface AppointmentsListProps {
 export function AppointmentsList({ times }: AppointmentsListProps) {
   const searchParams = useSearchParams()
   const date = searchParams.get('date')
-  console.log(date)
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['get-appointments', date],
+    queryFn: async () => {
+      let activeDate = date
+
+      if (!activeDate) {
+        const today = format(new Date(), 'yyyy-MM-dd')
+        activeDate = today
+      }
+
+      const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/clinic/appointments?date=${activeDate}`
+
+      const response = await fetch(url)
+
+      const json = (await response.json()) as AppointmentWithService[]
+
+      console.log('appointments', json)
+
+      if (!response.ok) {
+        return []
+      }
+
+      return json
+    },
+  })
+
+  const occupantMap: Record<string, AppointmentWithService> = {}
+
+  if (data && data.length > 0) {
+    for (const appointment of data) {
+      const requiredSlots = Math.ceil(appointment.service.duration / 30)
+
+      const startIndex = times.indexOf(appointment.time)
+
+      if (startIndex !== -1) {
+        for (let i = 0; i < requiredSlots; i++) {
+          const slotIndex = startIndex + i
+
+          if (slotIndex < times.length) {
+            occupantMap[times[slotIndex]] = appointment
+          }
+        }
+      }
+    }
+  }
+
   return (
     <Card className="gap-4">
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
@@ -24,15 +77,30 @@ export function AppointmentsList({ times }: AppointmentsListProps) {
 
       <CardContent>
         <ScrollArea className="h-[calc(100vh-20rem)] lg:h-[calc(100vh-15rem)] pr-4">
-          {times.map((slot) => (
-            <div
-              key={slot}
-              className="flex items-center py-2 border-t last:border-b"
-            >
-              <div className="w-16 text-sm font-semibold">{slot}</div>
-              <div className="flex-1 text-sm text-gray-500">Disponivel</div>
-            </div>
-          ))}
+          {isLoading ? (
+            <p>Carregando agenda</p>
+          ) : (
+            times.map((slot) => {
+              const occupant = occupantMap[slot]
+
+              if (occupant) {
+                return (
+                  <div
+                    key={slot}
+                    className="flex items-center py-2 border-t last:border-b"
+                  >
+                    <div className="w-16 text-sm font-semibold">{slot}</div>
+                    <div className="flex-1 text-sm">
+                      <div className="font-semibold">{occupant.name}</div>
+                      <div className="text-sm text-gray-500">
+                        {occupant.phone}
+                      </div>
+                    </div>
+                  </div>
+                )
+              }
+            })
+          )}
         </ScrollArea>
       </CardContent>
     </Card>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { SessionAuthProvider } from '@/components/session-auth'
 import { Toaster } from 'sonner'
+import { QueryClientContext } from '@/providers/queryClient'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,8 +32,10 @@ export default function RootLayout({
         suppressHydrationWarning
       >
         <SessionAuthProvider>
-          <Toaster duration={2500} />
-          {children}
+          <QueryClientContext>
+            <Toaster duration={2500} />
+            {children}
+          </QueryClientContext>
         </SessionAuthProvider>
       </body>
     </html>
