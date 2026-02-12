d7e7bb7f2f6f070a662b5487bf2254ce3b42a1be
sayro <sayrodamaceno70@gmail.com>
2026-02-12 17:54:44 -0300

    feat: add user permission check for reports access based on subscription plan

---
+++ src/app/(panel)/dashboard/reports/page.tsx
@@ -0,0 +1,26 @@
+import { redirect } from 'next/navigation'
+import { getPermissionUserToReports } from './_data-access/get-permissions-reports'
+import getSession from '@/lib/getSession'
+
+export default async function Reports() {
+  const session = await getSession()
+
+  if (!session) {
+    redirect('/')
+  }
+
+  const user = await getPermissionUserToReports({ userId: session.user.id })
+
+  if (!user) {
+    return (
+      <main>
+        <h1>Você não tem permissão para acessar esta página</h1>
+        <p>
+          Assine o plano <strong>PROFISSIONAL</strong> para ter acesso completo!
+        </p>
+      </main>
+    )
+  }
+
+  return <div>Página de Relatórios</div>
+}
